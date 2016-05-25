8205e0d5cff22dc8f94dfdfcfde339272f3f9130
Andy Richardson <rishson@gmail.com>
2016-05-25 16:10:41 +0100

    main

---
+++ main.js
@@ -0,0 +1,32 @@
+
+var _ = require('lodash');
+
+function main(){
+
+  function goodCheck(a, b){
+    return a === b;
+  }
+
+
+  function badCheck(a, b){
+    return a == b;
+  }
+
+  var isSame = badCheck(10, 20);
+  var isSameAgain = goodCheck(10, 20);
+  console.log(isSame);
+
+  function doSomething(){
+    //TODO make this do something
+    console.log('i dont really do alot :-(');
+  }
+
+  var x = add(1,2);
+  var y = add(15,2);
+
+  // TODO implement add with as many JS gotchas as possible
+  function add(x, y) {
+  }
+}
+
+main();
